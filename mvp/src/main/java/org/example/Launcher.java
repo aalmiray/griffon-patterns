e4efa4414e93644f1f1ac733c48156377cda96c1
aalmiray <aalmiray@gmail.com>
2015-12-15 04:14:11 +0100

    Publish of Github pages from Gradle.

--- mvp/src/main/java/org/example/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import griffon.javafx.JavaFXGriffonApplication;
-
-public class Launcher {
-    public static void main(String[] args) throws Exception {
-        JavaFXGriffonApplication.main(args);
-    }
-}
